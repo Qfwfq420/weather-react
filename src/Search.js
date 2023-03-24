bfc739fa93c568ae63c3cdabb426e37a011dc64e
Qfwfq420 <mahtabzarrinpashneh@gmail.com>
2023-03-24 23:57:05 +0430

    debug

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function (props) {
+export default function Search(props) {
   let [city, setCity] = useState("");
   let [temp, setTemp] = useState(null);
   let [desc, setDesc] = useState(null);
